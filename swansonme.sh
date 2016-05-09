e39affeecbe1d8c5951a5db14001800d1b96b16b
Steve McIntosh <scm.phub@gmail.com>
2016-05-09 15:52:47 -0400

    swanson me

--- swansonme.sh
+++ swansonme.sh
@@ -2,8 +2,8 @@
 
 
 quote="$(curl -s http://ron-swanson-quotes.herokuapp.com/v2/quotes)"
-echo *${quote:1:${#quote}-2}* -R. Swanson
-
+#echo *${quote:1:${#quote}-2}* -R. Swanson
+echo Here is where the Swanson quote will go.
 
 # bold the quote, add -R. Swanson
 
